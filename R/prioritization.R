4abb781a3c132315aa72dd1a4c731246b3f29117
huerqiang <13766876214@163.com>
2019-12-31 11:16:41 +0800

    fix a bug

--- R/prioritization.R
+++ R/prioritization.R
@@ -5,10 +5,6 @@
 #' @param node2 a gene
 #'
 #' @return a number
-#' @export
-#'
-#' @examples
-#' get_W('hh','dd')
 get_W <- function(node1, node2) {
     node <- paste(sort(c(node1, node2)), collapse = "_")
     return(as.numeric(ifelse(node %in% rownames(edge_weight), 
@@ -21,10 +17,6 @@ get_W <- function(node1, node2) {
 #' @param net a network
 #'
 #' @return a vector of gene
-#' @export
-#'
-#' @examples
-#' get_neighbor('LPL',net_disease)
 get_neighbor <- function(node, net) {
     aa1 <- which(net[, 1] == node)
     aa2 <- which(net[, 2] == node)
@@ -34,12 +26,11 @@ get_neighbor <- function(node, net) {
 
 #' Title get the disease risk transition probability matrix
 #'
+#' @param node_weight a matrix, genes and their weights
+#' @param net_disease_term GO terms for each pair of nodes in the network
 #' @return a matrix
-#' @export
-#'
-#' @examples
-#' get_Q()
-get_Q <- function() {
+
+get_Q <- function(node_weight, net_disease_term) {
     Q <- matrix(0, dim(node_weight)[1], dim(node_weight)[1])
     rownames(Q) <- node_weight[, 1]
     colnames(Q) <- node_weight[, 1]
@@ -88,19 +79,24 @@ get_R_0 <- function(disease_gene, node_weight, f = 1) {
 }
 #' Title get the final genetic disease risk scores
 #'
-#' @param Q  the disease risk transition probability matrix
 #' @param bet  a parameter to measure the importance of genes and interactions
 #' @param R_0 the vector of initial disease risk scores for all genes
 #' @param node_weight a matrix, genes and their weights
 #' @param threshold a threshold for terminating iterations
-#'
+#' @param net_disease_term GO terms for each pair of nodes in the network
 #' @return a matrix
 #' @export
 #'
 #' @examples
-#'get_R(Q,0.5,R_0,node_weight,10^(-9))
-get_R <- function(Q, bet, R_0, node_weight, threshold = 10^(-9)) {
-    QQ <- Q
+#' net_disease <- deal_net(net,dise_gene)
+#' genes_mat <- get_gene_mat(net_disease,GO_human)
+#' node_weight <- get_node_weight(genes_mat)
+#' net_disease_term <- get_net_disease_term(genes_mat,net_disease)
+#' R_0<- get_R_0(dise_gene,node_weight,f=1)
+#' result <- get_R(node_weight, net_disease_term, bet = 0.5, R_0 = R_0, threshold = 10^(-9))
+get_R <- function(node_weight, net_disease_term, bet, R_0, threshold = 10^(-9)) {
+    
+    QQ <- get_Q(node_weight, net_disease_term)
     QQ[is.na(QQ)] <- 0
     R_dise <- 1
     R_old <- R_0
